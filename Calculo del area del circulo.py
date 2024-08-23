f4b0ed45c278f99343f2b2740d42aeaed9de3b8b
McFume <marcelorojas05@gmail.com>
2024-08-23 16:44:13 -0600

    Add files via upload

---
+++ Calculo del area del circulo.py
@@ -0,0 +1,7 @@
+print('Programa que calcula el area de un circulo')
+
+radio = float(input('Ingrese el radio del circulo \t'))
+
+area = 3.14 * radio ** 2
+
+print('El area del circulo es de', area)
