c1b455b644432ae92f3847a0037f17958e546abf
McFume <marcelorojas05@gmail.com>
2024-08-23 14:07:42 -0600

    Add files via upload

---
+++ Determinacion del mayor de dos numeros.py
@@ -0,0 +1,11 @@
+print('Este programa determina cual de los 2 numeros ingresados es el mayor')
+
+n = float(input('Ingrese el primer numero \t'))
+
+m = float(input('Ingrese el segundo numero \t'))
+
+if n > m:
+    print('El numero mas grande es el', n)
+    
+else:
+    print('El numero mas grande es el', m)
