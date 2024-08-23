9fd2355ad5c4414c410c3e478cc37712d9da56a6
McFume <marcelorojas05@gmail.com>
2024-08-23 17:07:59 -0600

    Add files via upload

---
+++ Calculo del perimetro y el area de un rectangulo.py
@@ -0,0 +1,11 @@
+print('Programa que calcula el perimetro y area de un rectangulo')
+
+ancho = float(input('Ingrese el ancho del rectangulo \t'))
+
+altura = float(input('Ingrese la altura del rectangulo \t'))
+
+perimetro = 2 * (ancho + altura)
+
+area = ancho * altura
+
+print("El area del rectangulo es de", area ,"y su perimetro es de", perimetro)
