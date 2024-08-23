66288df205455acdcc97123dbb947371072d9a26
McFume <marcelorojas05@gmail.com>
2024-08-23 16:19:31 -0600

    Add files via upload

---
+++ Conversion de temperatura de celciuc a fahrenheit.py
@@ -0,0 +1,7 @@
+print('Este programa convierte grados celsius a fahrenheit')
+
+celsius = float(input('Ingrese el numero de grados celsius que desea convertir a fahrenheit \t'))
+
+fahrenheit = celsius * (9/5) + 32
+
+print('La cantidad convertida de celsius a fahrenheit es de', fahrenheit)
