a1fa6c0bc9f07d2e3956a53685ebc1c1683fea5a
McFume <marcelorojas05@gmail.com>
2024-08-23 13:54:06 -0600

    Add files via upload

---
+++ Calculo del promedio de tres numeros.py
@@ -0,0 +1,11 @@
+print('Ingrese 3 numeros para poder calcular su promedio')
+
+n = float(input('Ingrese el primer numero \t'))
+
+b = float(input('Ingrese el segundo numero \t'))
+
+m = float(input('Ingrese el tercer numero \t'))
+
+promedio = (n + b + m)/3
+
+print('El promedio de estos 3 numeros es de', promedio)
