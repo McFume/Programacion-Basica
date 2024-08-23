c58dede5bbdfef8e0ee2dae8a34ad65c6b4e1393
McFume <marcelorojas05@gmail.com>
2024-08-23 17:30:35 -0600

    Add files via upload

---
+++ Conversion de horas a minutos.py
@@ -0,0 +1,7 @@
+print('Programa que convierte horas a minutos')
+
+horas = int(input('Ingrese la cantidad de horas que desea convertir a minutos \t'))
+
+minutos = horas * 60
+
+print('La cantidad de horas convertida en minutos es de', minutos)
