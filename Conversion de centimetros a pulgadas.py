fae198cc80ec9efebd605af5c0f35a5463c11177
McFume <marcelorojas05@gmail.com>
2024-08-23 15:38:48 -0600

    Add files via upload

---
+++ Conversion de centimetros a pulgadas.py
@@ -0,0 +1,7 @@
+print('Este programa convierte una cantidad dada de centimetros a pulgadas')
+
+cm = float(input('Ingrese la cantidad de cm que desea convertir \t'))
+
+pulgadas = cm / 2.54
+
+print('La conversion de centimetros a pulgadas es de', pulgadas)
