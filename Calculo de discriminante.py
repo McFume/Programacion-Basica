4f6ebc5e2fd922b7604c7c3ef1f8dba9c1c9dd4e
McFume <marcelorojas05@gmail.com>
2024-08-23 20:57:57 -0600

    Add files via upload

---
+++ Calculo de discriminante.py
@@ -0,0 +1,11 @@
+print('Programa que calcula la discriminante de una ecuacion de segundo grado')
+
+a = float(input('Ingrese la variable a de la ecuacion \t'))
+
+b = float(input('Ingrese la variable b de la ecuacion \t'))
+
+c = float(input('Ingrese la variable c de la ecuacion \t'))
+
+discriminante = (b ** 2) - 4 * a * c
+
+print('La discriminante de la ecuacion es de', discriminante )
